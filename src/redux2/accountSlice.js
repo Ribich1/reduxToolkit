5b8c9fb91c42fd82c928e683e2fd236dd394a91d
Ribich1 <ribich@ukr.net>
2024-06-14 21:38:10 +0200

    createReducer

--- src/redux2/accountSlice.js
+++ src/redux2/accountSlice.js
@@ -1,19 +1,30 @@
-export const accountReducer = (state = { balance: 0 }, action) => {
-  switch (action.type) {
-    case 'account/deposit':
-      return { ...state, balance: state.balance + action.payload };
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-    default:
-      return state;
-  }
-};
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
+export const deposit = createAction('account/deposit');
 
-export const deposit = value => {
-  return { type: 'account/deposit', payload: value };
-};
+export const withdraw = createAction('account/withdraw');
 
-export const withdraw = value => {
-  return { type: 'account/withdraw', payload: value };
-};
+export const accountReducer = createReducer({ balance: 0 }, builder =>
+  builder
+    .addCase(deposit, (state, action) => {
+      state.balance += action.payload;
+      // return { ...state, balance: state.balance + action.payload };
+    })
+    .addCase(withdraw, (state, action) => {
+      state.balance -= action.payload;
+      // return { ...state, balance: state.balance - action.payload };
+    })
+);
+
+// 25
+
+// export const accountReducer = (state = { balance: 0 }, action) => {
+//   switch (action.type) {
+//     case 'account/deposit':
+//       return { ...state, balance: state.balance + action.payload };
+//     case 'account/withdraw':
+//       return { ...state, balance: state.balance - action.payload };
+//     default:
+//       return state;
+//   }
+// };
