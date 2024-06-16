295b902a2399f413a96fe6075a594949a9b981da
Ribich1 <ribich@ukr.net>
2024-06-16 22:21:06 +0200

    toolKit

--- src/redux2/accountSlice.js
+++ src/redux2/accountSlice.js
@@ -1,24 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// export const deposit = createAction('account/deposit');
-
-// export const withdraw = createAction('account/withdraw');
-
-// export const accountReducer = createReducer({ balance: 0 }, builder =>
-//   builder
-//     .addCase(deposit, (state, action) => {
-//       state.balance += action.payload;
-//       // return { ...state, balance: state.balance + action.payload };
-//     })
-//     .addCase(withdraw, (state, action) => {
-//       state.balance -= action.payload;
-//       // return { ...state, balance: state.balance - action.payload };
-//     })
-// );
-
-
-// 35
-
 const slice = createSlice({
   name: 'account',
   initialState: { balance: 0 },
@@ -32,16 +13,25 @@ const slice = createSlice({
   },
 });
 
+// const slice = createSlice({
+//   name: 'account',
+//   initialState: { balance: 0 },
+//   reducers: {
+//     deposit:{reducer(state, action) {
+//       state.balance += action.payload;
+//     }, prepare(value) {
+//       return {
+//         payload:{value,id:Date.now()}
+//       }
+//     }},
+//     withdraw(state, action) {
+//       state.balance -= action.payload;
+//     },
+//   },
+// });
+
+
 export const { deposit, withdraw } = slice.actions;
 export const accountReducer = slice.reducer;
 
-// export const accountReducer = (state = { balance: 0 }, action) => {
-//   switch (action.type) {
-//     case 'account/deposit':
-//       return { ...state, balance: state.balance + action.payload };
-//     case 'account/withdraw':
-//       return { ...state, balance: state.balance - action.payload };
-//     default:
-//       return state;
-//   }
-// };
+console.log(deposit(5))
